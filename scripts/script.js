04b6a642b0ac09378e53beb2ce532026bfec7c01
glptrst <giulio.pietroiusti@gmail.com>
2017-04-11 17:08:42 +0200

    Make characters fit into the screen
    Add css to prevent characters from going outside the screen.
    Add (rudimentary) function to adapt size of characters give
    the their number.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+"use strict";
 window.onload = function () {
     var totalString = "0";
     var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -12,6 +13,14 @@ window.onload = function () {
     function update() {
         var screen = document.getElementById("steps");
         screen.innerHTML = totalString;
+
+        //adapt font-size
+        if (totalString.length <= 10)
+            screen.setAttribute("style", "font-size: 50px");
+        else if (totalString.length > 10 && totalString.length <= 75)
+            screen.setAttribute("style", "font-size: 20px");
+        else if (totalString.length > 75)
+            screen.setAttribute("style", "font-size: 15px");
     }
     //manage input received
     function getValue(input) {
@@ -24,7 +33,7 @@ window.onload = function () {
         } else if (input == "AC"){ 
             totalString = "0";
             update();
-        } else if (input === "Back") {
+        } else if (input === "CE") {
             if (totalString === "0") {
                 ; //do nothing
             } else {
@@ -45,11 +54,23 @@ window.onload = function () {
         }
     }
     //call getValue with the right arg when a button is clicked
-    var buttons = document.getElementsByClassName("button");
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", function whenclicked(){
-            console.log(this.id);
-            getValue(this.id);
-        });
+    //(I use an IIFE to not pollute the scope with i)
+    (function () {    
+        var buttons = document.getElementsByClassName("button");
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener("click", function whenclicked(){
+                console.log(this.id);
+                getValue(this.id);
+            });
+        }
+    })();
+    
+    //rudimentary function to change size of character to make them not overflow the screen
+    function adaptFontSize() {
+        if (totalString.length > 10) {
+            var screen = document.getElementById("steps");
+            screen.setAttribute("font-size", "20px");
+        }
     }
+
 };
